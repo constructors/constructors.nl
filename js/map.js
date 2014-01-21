9c078b71faa13a108991c0fcf15f12382d84208c
Joris van Rooij <jorrizza@jrrzz.net>
2014-01-21 18:39:55 +0100

    Some beginnings of a contact page.

---
+++ js/map.js
@@ -0,0 +1,11 @@
+$(function () {
+  new GMaps({
+    div: '#gmap',
+    lat: 51.4348186,
+    lng: 5.4778511
+  }).addMarker({
+    lat: 51.4348186,
+    lng: 5.4778511,
+    title: 'Constructors'
+  });
+});
