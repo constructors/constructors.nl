4de055d29362535449be3ba79c5b5f2de4568231
Joris van Rooij <jorrizza@jrrzz.net>
2014-03-05 22:51:18 +0100

    New map tiles.

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
 $(function () {
   var map = L.map('cmap').setView([51.4348186, 5.4778511], 15);
 
-  L.tileLayer('http://{s}.tile.cloudmade.com/5b2cd43f637d474f88eaddfedf7de761/997/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://cloudmade.com">CloudMade</a>',
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18
   }).addTo(map);
 
