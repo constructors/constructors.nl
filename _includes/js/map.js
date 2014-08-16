69250eebed3b7f571a186a0ac4ca277b392c48cc
Bjorn Heesakkers <bjorn@expleo.nl>
2014-08-16 21:03:54 +0200

    Fixed openstreetmaps?

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -1,8 +1,8 @@
 $(function () {
   var map = L.map('cmap').setView([51.4348186, 5.4778511], 15);
 
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18
   }).addTo(map);
 
