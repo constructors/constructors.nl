919b27d2c8d52cbb62fa02707e8d7fa11461403b
Bjorn Heesakkers <bjorn@expleo.nl>
2014-08-16 21:04:56 +0200

    That didn't work

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -1,7 +1,7 @@
 $(function () {
   var map = L.map('cmap').setView([51.4348186, 5.4778511], 15);
 
-  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18
   }).addTo(map);
