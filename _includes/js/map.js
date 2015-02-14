f23365511aac8a88000aaa7d4b71aa12518b70db
Joris van Rooij <jorrizza@jrrzz.net>
2015-02-14 14:16:56 +0100

    Bunch of updates and a new office location.

--- _includes/js/map.js
+++ _includes/js/map.js
@@ -1,10 +1,10 @@
 $(function () {
-  var map = L.map('cmap').setView([51.4348186, 5.4778511], 15);
+  var map = L.map('cmap').setView([51.44509, 5.44728], 15);
 
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18
   }).addTo(map);
 
-  var marker = L.marker([51.4348186, 5.4778511]).addTo(map);
+  var marker = L.marker([51.44509, 5.44728]).addTo(map);
 });
